f42001d610bfd5b05787564faff7378574331e22
harrisap <harrisap@rose-hulman.edu>
2018-10-03 08:23:47 -0400

    Coded

--- src/rosebotics.py
+++ src/rosebotics.py
@@ -22,9 +22,9 @@ class Snatch3rRobot(object):
         self.left_wheel.start_spinning(left_wheel_duty_cycle_percent)
         self.right_wheel.start_spinning(right_wheel_duty_cycle_percent)
 
-    def stop(self, stop_action=StopAction.BRAKE):
-        self.left_wheel.stop_spinning(stop_action.BRAKE)
-        self.right_wheel.stop_spinning(stop_action.BRAKE)
+    def stop(self, stop_action=StopAction.BRAKE.value):
+        self.left_wheel.stop_spinning(stop_action)
+        self.right_wheel.stop_spinning(stop_action)
 
 class Wheel(object):
     def __init__(self, port, default_duty_cycle_percent=100,
